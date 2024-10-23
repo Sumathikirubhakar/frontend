8f78854c23018950b51bf9f11ccdba770758e557
Sumathikirubhakar <kirubhakarme14101994@gmail.com>
2024-10-23 12:04:25 +0530

    frontend backend ready

---
+++ src/Components/Fronrtendbackendconnection/Forms.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import './Forms.css'
+
+export default function Forms() {
+  return (
+    
+    <div>
+      <div class="container">
+        <h2>FRONTEND & BACKEND CONNECTION</h2>
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Title</th>
+              <th>Sub-Title</th>
+              <th>Image</th>
+              <th>Action</th>
+              
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
+
